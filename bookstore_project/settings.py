c4e8791dd37b9f0466bec757593b0c202f8291ac
Barun <bhattacharjeebarun25@gmail.com>
2020-10-03 19:42:14 +0600

    c2

--- bookstore_project/settings.py
+++ bookstore_project/settings.py
@@ -82,8 +82,8 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4' # need to specify the CSS framework you want
 
 MIDDLEWARE = [
     'django.middleware.cache.UpdateCacheMiddleware', # to add per-site caching
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
